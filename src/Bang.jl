f883ed114b985ca6610610e5a65bd54a4fa60955
4thjuly <4thjuly@users.noreply.github.com>
2020-10-20 00:12:18 -0700

    ...

--- src/Bang.jl
+++ src/Bang.jl
@@ -2,6 +2,7 @@ module Bang
 
 export @!
 
+# Call first param as function with remaining params as args eg @! cd ".." becomes cd("..")
 macro !(f, args...) Expr(:call, f, args...) end
 
 end # module
